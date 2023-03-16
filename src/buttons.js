cad7a2ee59be7cecb5886558214225f951961001
Jereen <jader9e1@hotmail.com>
2023-03-15 21:13:20 -0500

    "feat: Add buttons js json objects"

---
+++ src/buttons.js
@@ -0,0 +1,84 @@
+const buttons = [
+  {
+    id: 1,
+    name: "√"
+  },
+  {
+    id: 2,
+    name: "^"
+  },
+  {
+    id: 3,
+    name: "clear"
+  },
+  {
+    id: 4,
+    name: 1
+  },
+  {
+    id: 5,
+    name: 2
+  },
+
+  {
+    id: 6,
+    name: 3
+  },
+  {
+    id: 7,
+    name: 4
+  },
+  {
+    id: 8,
+    name: 5
+  },
+  {
+    id: 9,
+    name: 6
+  },
+  {
+    id: 10,
+    name: 7
+  },
+  {
+    id: 11,
+    name: 8
+  },
+  {
+    id: 12,
+    name: 9
+  },
+  {
+    id: 13,
+    name: 0
+  },
+  {
+    id: 14,
+    name: "."
+  },
+  {
+    id: 15,
+    name: "(-)"
+  },
+  {
+    id: 16,
+    name: "+"
+  },
+  {
+    id: 17,
+    name: "-"
+  },
+  {
+    id: 18,
+    name: "/"
+  },
+  {
+    id: 19,
+    name: "*"
+  },
+  {
+    id: 20,
+    name: "="
+  }
+];
+export default buttons;
